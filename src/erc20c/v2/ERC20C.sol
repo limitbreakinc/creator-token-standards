1fdfe7637030617a49a4cdf1d0b2405b47a5c34d
Nathan Gang <nathan.gang@limitbreak.com>
2023-11-08 14:58:27 -0700

    Add ERC20-C Standard

---
+++ src/erc20c/v2/ERC20C.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "src/utils/CreatorTokenBaseV2.sol";
+import "src/token/erc20/ERC20OpenZeppelin.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+
+/**
+ * @title ERC20C
+ * @author Limit Break, Inc.
+ * @notice Extends OpenZeppelin's ERC20 implementation with Creator Token functionality, which
+ *         allows the contract owner to update the transfer validation logic by managing a security policy in
+ *         an external transfer validation security policy registry.  See {CreatorTokenTransferValidator}.
+ */
+abstract contract ERC20C is ERC165, ERC20OpenZeppelin, CreatorTokenBaseV2 {
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+        return 
+        interfaceId == type(IERC20).interfaceId || 
+        interfaceId == type(IERC20Metadata).interfaceId || 
+        interfaceId == type(ICreatorToken).interfaceId || 
+        interfaceId == type(ICreatorTokenV2).interfaceId || 
+        super.supportsInterface(interfaceId);
+    }
+
+    /// @dev Ties the open-zeppelin _beforeTokenTransfer hook to more granular transfer validation logic
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 /*amount*/) internal virtual override {
+        _validateBeforeTransfer(from, to, 0);
+    }
+
+    /// @dev Ties the open-zeppelin _afterTokenTransfer hook to more granular transfer validation logic
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 /*amount*/) internal virtual override {
+        _validateAfterTransfer(from, to, 0);
+    }
+}
+
+/**
+ * @title ERC20CInitializable
+ * @author Limit Break, Inc.
+ * @notice Initializable implementation of ERC20C to allow for EIP-1167 proxy clones.
+ */
+abstract contract ERC20CInitializable is ERC165, ERC20OpenZeppelinInitializable, CreatorTokenBaseV2 {
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+        return 
+        interfaceId == type(IERC20).interfaceId || 
+        interfaceId == type(IERC20Metadata).interfaceId || 
+        interfaceId == type(ICreatorToken).interfaceId || 
+        interfaceId == type(ICreatorTokenV2).interfaceId || 
+        super.supportsInterface(interfaceId);
+    }
+
+    /// @dev Ties the open-zeppelin _beforeTokenTransfer hook to more granular transfer validation logic
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 /*amount*/) internal virtual override {
+        _validateBeforeTransfer(from, to, 0);
+    }
+
+    /// @dev Ties the open-zeppelin _afterTokenTransfer hook to more granular transfer validation logic
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 /*amount*/) internal virtual override {
+        _validateAfterTransfer(from, to, 0);
+    }
+}
---
+++ src/token/erc20/ERC20OpenZeppelin.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "../../access/OwnablePermissions.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+abstract contract ERC20OpenZeppelinBase is ERC20 {
+
+    // Token name
+    string internal _contractName;
+
+    // Token symbol
+    string internal _contractSymbol;
+
+    function name() public view virtual override returns (string memory) {
+        return _contractName;
+    }
+
+    function symbol() public view virtual override returns (string memory) {
+        return _contractSymbol;
+    }
+
+    function _setNameAndSymbol(string memory name_, string memory symbol_) internal {
+        _contractName = name_;
+        _contractSymbol = symbol_;
+    }
+}
+
+abstract contract ERC20OpenZeppelin is ERC20OpenZeppelinBase {
+    constructor(string memory name_, string memory symbol_) ERC20("", "") {
+        _setNameAndSymbol(name_, symbol_);
+    }
+}
+
+abstract contract ERC20OpenZeppelinInitializable is OwnablePermissions, ERC20OpenZeppelinBase {
+
+    error ERC20OpenZeppelinInitializable__AlreadyInitializedERC20();
+
+    /// @notice Specifies whether or not the contract is initialized
+    bool private _erc20Initialized;
+
+    /// @dev Initializes parameters of ERC721 tokens.
+    /// These cannot be set in the constructor because this contract is optionally compatible with EIP-1167.
+    function initializeERC20(string memory name_, string memory symbol_) public {
+        _requireCallerIsContractOwner();
+
+        if(_erc20Initialized) {
+            revert ERC20OpenZeppelinInitializable__AlreadyInitializedERC20();
+        }
+
+        _erc20Initialized = true;
+
+        _setNameAndSymbol(name_, symbol_);
+    }
+}
