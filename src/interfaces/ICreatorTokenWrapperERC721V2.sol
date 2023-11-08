4ba878d0fbdb00da17f47e24f62d9a11a17b43ee
Nathan Gang <nathan.gang@limitbreak.com>
2023-11-08 11:49:31 -0700

    Implement V2 721C and 1155C contracts, with V2 examples

---
+++ src/interfaces/ICreatorTokenWrapperERC721V2.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./ICreatorTokenV2.sol";
+
+interface ICreatorTokenWrapperERC721V2 is ICreatorTokenV2 {
+
+    event Staked(uint256 indexed tokenId, address indexed account);
+    event Unstaked(uint256 indexed tokenId, address indexed account);
+    event StakerConstraintsSet(StakerConstraints stakerConstraints);
+
+    function stake(uint256 tokenId) external payable;
+    function unstake(uint256 tokenId) external payable;
+    function canUnstake(uint256 tokenId) external view returns (bool);
+    function getStakerConstraints() external view returns (StakerConstraints);
+    function getWrappedCollectionAddress() external view returns (address);
+}
