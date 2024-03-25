4be744ada7f2a8b3cf3f1edb3e41f0eeee48669b
Nathan Gang <nathan.gang@limitbreak.com>
2024-03-25 16:07:47 -0600

    Update interface

--- src/interfaces/ICreatorToken.sol
+++ src/interfaces/ICreatorToken.sol
@@ -4,5 +4,7 @@ pragma solidity ^0.8.4;
 interface ICreatorToken {
     event TransferValidatorUpdated(address oldValidator, address newValidator);
     function getTransferValidator() external view returns (address validator);
+    function isTransferAllowed(address caller, address from, address to) external view returns (bool);
+    function isTransferAllowed(address caller, address from, address to, uint256 tokenId) external view returns (bool);
     function setTransferValidator(address validator) external;
 }
