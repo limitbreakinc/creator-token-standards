97c1333e9e2afcc3ce3357f67c38800bd423a0dc
Nathan Gang <nathan.gang@limitbreak.com>
2024-03-29 16:01:54 -0600

    Add folders to ignore for code coverage

--- script/test/generate-coverage-report.sh
+++ script/test/generate-coverage-report.sh
@@ -1,6 +1,6 @@
 forge coverage --ir-minimum --report lcov
 
-lcov --remove ./lcov.info -o ./lcov.info.pruned '/test/mocks/*' 'test/mocks/*'
+lcov --remove ./lcov.info -o ./lcov.info.pruned '/test/mocks/*' 'test/mocks/*' '/test/adventures/*' 'test/adventures/*' '/test/utils/*' 'test/utils/*' '/script/common/*' 'script/common/*' 
 
 genhtml lcov.info.pruned --output-directory coverage
 
