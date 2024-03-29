4c61c3342da1154b982387889639200321920c1f
Nathan Gang <nathan.gang@limitbreak.com>
2024-03-29 15:08:55 -0600

    Finished getting compiles working

--- script/common/DeployValidator.s.sol
+++ script/common/DeployValidator.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.4;
 
 import "forge-std/Script.sol";
-import "src/utils/CreatorTokenTransferValidatorV2.sol";
+import "src/utils/CreatorTokenTransferValidator.sol";
 
-contract DeployV2 is Script {
+contract DeployValidator is Script {
     function run() public {
         bytes32 saltValue = bytes32(vm.envUint("SALT_TRANSFER_VALIDATOR_V2"));
-        address expectedAddress = vm.envAddress("EXPECTED_VALIDATOR_ADDRESS_V2");
+        address expectedAddress = vm.envAddress("EXPECTED_VALIDATOR_ADDRESS");
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_KEY");
         address defaultOwner = vm.envAddress("DEFAULT_OWNER_ADDRESS");
         
         vm.startBroadcast(deployerPrivateKey);
-        address validator = address(new CreatorTokenTransferValidatorV2{salt: saltValue}(defaultOwner));
+        address validator = address(new CreatorTokenTransferValidator{salt: saltValue}(defaultOwner, address(0), "", ""));
         vm.stopBroadcast();
 
         console.log("CreatorTokenTransferValidatorV2: ", validator);
